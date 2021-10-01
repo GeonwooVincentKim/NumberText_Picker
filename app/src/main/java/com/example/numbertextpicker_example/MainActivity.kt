e7566b48703351c6d7c33d681323ccbfd2c793da
GeonwooVincentKim <kdsnop@gmail.com>
2021-10-01 09:53:06 +0900

    [Feature] - Generated the page

--- app/src/main/java/com/example/numbertextpicker_example/MainActivity.kt
+++ app/src/main/java/com/example/numbertextpicker_example/MainActivity.kt
@@ -2,10 +2,29 @@ package com.example.numbertextpicker_example
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.NumberPicker
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private val showResultText: TextView by lazy {
+        findViewById(R.id.showResultText)
+    }
+
+    private val scrollNumberPicker: NumberPicker by lazy {
+        findViewById(R.id.scrollNumberPicker)
+//        findViewById<NumberPicker>(R.id.scrollNumberPicker)
+//            .apply{
+//                minValue = 5
+//                maxValue = 10
+//            }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        showResultText
+        scrollNumberPicker.minValue = 5
+        scrollNumberPicker.maxValue = 10
     }
 }
